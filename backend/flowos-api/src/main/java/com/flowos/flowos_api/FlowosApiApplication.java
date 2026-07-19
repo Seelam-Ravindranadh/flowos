526bd25b51a362b90cb30c35bdb5dbcd412c2f64
Seelam Ravindranadh <seelamravindranadh1998@gmail.com>
2026-07-19 22:41:39 +0530

    Created Spring Boot backend

---
+++ backend/flowos-api/src/main/java/com/flowos/flowos_api/FlowosApiApplication.java
@@ -0,0 +1,13 @@
+package com.flowos.flowos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowosApiApplication.class, args);
+	}
+
+}
---
+++ backend/flowos-api/src/test/java/com/flowos/flowos_api/FlowosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowos.flowos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
